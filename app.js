96dc38f5a6b46bec972f5f498a66999de4fa64f6
joao vitor <joaomakenze@gmail.com>
2024-01-02 16:21:12 -0300

    atualizando a tarefa

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-function salvar() {
-    let nome = document.getElementById('nome').value;
-    let numero = document.getElementById('numero').value;
-    alert(nome)
+    const nome = document.getElementById('nome').value;
+    const numero = document.getElementById('numero').value;
+
+    function salvar() {
+    document.getElementById('nome-salvo').innerHTML = `<td>${nome}</td>`;
+    document.getElementById('numero-salvo').innerHTML = `<td>${numero}</td>`;
+
 }
-setTimeout(salvar, 1000);
