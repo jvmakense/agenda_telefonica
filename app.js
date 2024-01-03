81acab7f80cced2d17188bbaba6c0aec2f5eaa3c
joao vitor <joaomakenze@gmail.com>
2024-01-03 17:10:42 -0300

    concluido a atividade

--- app.js
+++ app.js
@@ -1,8 +1,48 @@
-    const nome = document.getElementById('nome').value;
-    const numero = document.getElementById('numero').value;
+const listaContatos = [];
 
-    function salvar() {
-    document.getElementById('nome-salvo').innerHTML = `<td>${nome}</td>`;
-    document.getElementById('numero-salvo').innerHTML = `<td>${numero}</td>`;
+function adicionarContato() {
+    const nomeInput = document.getElementById('nome');
+    const telefoneInput = document.getElementById('telefone');
 
+    const nome = nomeInput.value;
+    const telefone = telefoneInput.value;
+
+    if (nome && telefone) {
+        listaContatos.push({ nome, telefone });
+        nomeInput.value = '';
+        telefoneInput.value = '';
+
+    
+    exibirContatos();
+    } 
+    else {
+    alert('Nome e número de telefone são obrigatórios.');
+    }
+}
+
+function exibirContatos() {
+    const corpoTabelaContatos = document.getElementById('corpoTabelaContatos');
+    corpoTabelaContatos.innerHTML = '';
+
+    for (const contato of listaContatos) {
+        const novaLinha = document.createElement('tr');
+        novaLinha.innerHTML = `<td>${contato.nome}</td><td>${contato.telefone}</td>`;
+    
+        const colunaAcao = document.createElement('td');
+        const botaoRemover = document.createElement('button');
+        botaoRemover.textContent = 'Remover';
+        botaoRemover.addEventListener('click', () => removerContato(contato.telefone));
+        colunaAcao.appendChild(botaoRemover);
+
+        novaLinha.appendChild(colunaAcao);
+        corpoTabelaContatos.appendChild(novaLinha);
+    }
+}
+
+function removerContato(telefone) {
+    const index = listaContatos.findIndex(contato => contato.telefone === telefone);
+        if (index !== -1) {
+        listaContatos.splice(index, 1);
+        exibirContatos(); 
+    }
 }
